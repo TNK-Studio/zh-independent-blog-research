681bd9edae0425dd78c60f87a51f86ae72747c26
mayne <dreama9ain@gmail.com>
2019-11-25 15:19:11 +0800

    添加导出数据方法

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
-from urllib.parse import urljoin
+import os
+import json
+from urllib.parse import urljoin, urlparse
 
 PASS_URL = ["youtube.com", "wikipedia.org", "facebook.com", "twitter.com", "zhihu.com",
             "weibo.com", "github.com", "reactjs.org", "material-ui.com", "gatsbyjs.org",
@@ -29,3 +31,43 @@ def rm_slash(url):
     if url.endswith('/'):
         return url[:-1]
     return url
+
+
+def to_json():
+    data = []
+    for itemdir in iter(os.listdir('data')):
+        with open('data/'+itemdir, 'r') as f:
+            data.append(json.load(f))
+
+    with open('data.json', 'w') as f:
+        json.dump(data, f)
+    # return data
+
+
+def to_graphql_data():
+    data = {'nodes': [], 'edges': []}
+
+    url_set = set()
+    url_friends_map = {}
+    for itemdir in iter(os.listdir('data')):
+        with open('data/'+itemdir, 'r') as f:
+            item = json.load(f)
+            url_set.add(item['url'])
+            data['nodes'].append({
+                'id': item['url'],
+                'label': item['url'],
+                'title': item['name'],
+                'group': item['url'].split(".")[-1]
+            })
+            url_friends_map[item['url']] = item['friends']
+
+    for url, friends in url_friends_map.items():
+        for friend in friends:
+            if friend in url_set:
+                data['edges'].append({
+                    'from': url,
+                    "to": friend
+                })
+
+    with open('site/src/data.json', 'w') as f:
+        json.dump(data, f)
