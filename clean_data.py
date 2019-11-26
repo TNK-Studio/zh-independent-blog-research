5f2768398e310036982a9eb394f87447e35d3a50
mayne <dreama9ain@gmail.com>
2019-11-26 10:40:13 +0800

    mv data_clean > clean_data

--- clean_data.py
+++ clean_data.py
@@ -15,8 +15,11 @@ class Cleaner:
                 print(f"{i} has been cleaned")
 
     def clean_run_status(self):
-
-        with open('run_status.json', 'w') as f:
+        """
+        如果 pass_domian 漏掉了某些 domain，后续的队列中可能出现一些需要过滤 domain 的站点。
+        为了提高效率，统一在此清理数据，而不是在抓取数据前作判断。
+        """
+        with open('run_status.json', 'r') as f:
             data = json.load(f)
             url_need_remove = []
             for url in iter(data['q']):
@@ -26,6 +29,8 @@ class Cleaner:
             for url in url_need_remove:
                 data['q'].remove(url)
                 print(f'{url} has been removed')
+        with open('run_status.json', 'w') as f:
+            json.dump(data,f)
 
 
 if __name__ == "__main__":
